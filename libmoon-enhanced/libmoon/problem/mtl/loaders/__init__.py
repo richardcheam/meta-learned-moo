3205e2997c61c5747cf8cc7346bd59e83163c339
Richard CHEAM <richard.cheam@ensiie.fr>
2026-02-06 23:17:43 +0100

    Remove duplicated libmoon directories

--- libmoon-enhanced/libmoon/problem/mtl/loaders/__init__.py
+++
@@ -1,5 +0,0 @@
-from .adult_loader import Adult
-from .compas_loader import Compas
-from .credit_loader import Credit
-
-from .multimnist_loader import MultiMNISTData
--- libmoon-enhanced/libmoon/problem/synthetic/__init__.py
+++
@@ -1,4 +0,0 @@
-from .vlmop import VLMOP1, VLMOP2
-from .zdt import ZDT1, ZDT2, ZDT3, ZDT4, ZDT6
-from .maf import MAF1
-
